ca1c4dcd1550e2dab8e17ecb3cbbf2843c06cdc8
dirkpr <dirk.p.reinhardt@ntnu.no>
2023-11-13 09:02:01 +0100

    rlmpc/common/policies.py: Move MPCPolicy here. Earlier in stable_baelines3 fork.

--- rlmpc/common/policies.py
+++ rlmpc/common/policies.py
@@ -15,6 +15,50 @@ from rlmpc.common.mpc import MPC
 from gymnasium import spaces
 
 
+class MPCPolicy(BasePolicy):
+    mpc: MPC
+    optimizer: th.optim.Optimizer
+
+    def __init__(
+        self,
+        observation_space: spaces.Space,
+        action_space: spaces.Space,
+        lr_schedule: Schedule,
+        mpc: MPC,
+        activation_fn: Type[nn.Module] = nn.Tanh,
+        ortho_init: bool = True,
+        use_sde: bool = False,
+        log_std_init: float = 0.0,
+        full_std: bool = True,
+        use_expln: bool = False,
+        squash_output: bool = False,
+        features_extractor_class: Type[BaseFeaturesExtractor] = FlattenExtractor,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        share_features_extractor: bool = True,
+        normalize_images: bool = True,
+        optimizer_class: Type[th.optim.Optimizer] = th.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        if optimizer_kwargs is None:
+            optimizer_kwargs = {}
+            # Small values to avoid NaN in Adam optimizer
+            if optimizer_class == th.optim.Adam:
+                optimizer_kwargs["eps"] = 1e-5
+
+        super().__init__(
+            observation_space,
+            action_space,
+            features_extractor_class,
+            features_extractor_kwargs,
+            optimizer_class=optimizer_class,
+            optimizer_kwargs=optimizer_kwargs,
+            squash_output=squash_output,
+            normalize_images=normalize_images,
+        )
+
+        self.mpc = mpc
+
+
 class MPCActorCriticPolicy(BasePolicy):
     mpc: MPC
     optimizer: th.optim.Optimizer
