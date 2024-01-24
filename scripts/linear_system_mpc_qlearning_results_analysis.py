161e34a875bbc4df95c78e2846a97208e4c89eb0
dirkpr <31849867+dirkpr@users.noreply.github.com>
2024-01-24 09:49:22 +0100

    Add some test functionality. Fix in parameter update for cartpole qlearning. (#10)
    * scripts/linear_system_mpc.py: Add nominal parameterized MPC implementation.
    
    * rlmpc/mpc/: Move all cartpole mpc functionality that is generic to higher level MPC class.
    
    * Implement Linear System for testing. Include slack in OCP formulation.
    
    * Include slack on state box constraints in NLP formulation.
    
    * Add plot to linear_systems_mpc.py
    
    * Implement qlearning for linear system.
    
    * scripts/cartpole_mpc_qlearning.py: Fix sign in parameter update.

---
+++ scripts/linear_system_mpc_qlearning_results_analysis.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    dataframe = pd.read_csv("/home/dirk/postdoc/rlmpc/data.csv")
+
+    print(dataframe.keys())
+
+    plt.figure(1)
+    plt.subplot(211)
+    dataframe.plot(y=["cost"], ax=plt.gca(), grid=True)
+    plt.subplot(212)
+    dataframe.plot(y=["td_error"], ax=plt.gca(), grid=True)
+
+    nrows = 5
+    plt.figure(2)
+    dataframe.plot(y=[f"A_{i}" for i in range(4)], ax=plt.subplot(nrows, 1, 1), grid=True)
+    dataframe.plot(y=[f"B_{i}" for i in range(2)], ax=plt.subplot(nrows, 1, 2), grid=True)
+    dataframe.plot(y=[f"b_{i}" for i in range(2)], ax=plt.subplot(nrows, 1, 3), grid=True)
+    dataframe.plot(y=[f"V_{i}" for i in range(1)], ax=plt.subplot(nrows, 1, 4), grid=True)
+    dataframe.plot(y=[f"f_{i}" for i in range(3)], ax=plt.subplot(nrows, 1, 5), grid=True)
+
+    plt.show()
