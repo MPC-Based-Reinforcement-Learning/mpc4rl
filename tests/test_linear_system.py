3c2772e672df3455ea9b4debcf95764b8d13bc40
dirk <dirk.p.reinhardt@ntnu.no>
2024-08-17 19:46:19 +0200

    Add closed_loop test to linear system tests

--- tests/test_linear_system.py
+++ tests/test_linear_system.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 from rlmpc.mpc.linear_system.acados import AcadosMPC
 
-
-from tests.common import (
+from common import (
     run_test_v_update_for_varying_parameters,
     run_test_q_update_for_varying_parameters,
     run_test_pi_update_for_varying_parameters,
@@ -14,16 +14,16 @@ from tests.common import (
 # from common import set_up_test_parameters
 
 
-param = {
-    "A": np.array([[1.0, 0.25], [0.0, 1.0]]),
-    # "B": np.array([[0.03125], [0.25]]),
-    "B": np.array([[0.03125], [0.25]]),
-    "Q": np.identity(2),
-    "R": 1e-2 * np.identity(1),
-    "b": np.array([[0.0], [0.0]]),
-    "f": np.array([[0.0], [0.0], [0.0]]),
-    "V_0": np.array([1e-3]),
-}
+# param = {
+#     "A": np.array([[1.0, 0.25], [0.0, 1.0]]),
+#     # "B": np.array([[0.03125], [0.25]]),
+#     "B": np.array([[0.03125], [0.25]]),
+#     "Q": np.identity(2),
+#     "R": 1e-2 * np.identity(1),
+#     "b": np.array([[0.0], [0.0]]),
+#     "f": np.array([[0.0], [0.0], [0.0]]),
+#     "V_0": np.array([1e-3]),
+# }
 
 kwargs = {
     "ocp_solver": {"json_file": "acados_ocp.json", "generate": True, "build": True},
@@ -87,11 +87,6 @@ def set_up_mpc(
     params = build_mpc_params()
     mpc = AcadosMPC(param=params, **kwargs)
 
-    # mpc.ocp_solver.solve()
-    # x0 = mpc.ocp_solver.get(0, "x")
-    # u0 = mpc.ocp_solver.get(0, "u")
-    # p0 = mpc.ocp_solver.acados_ocp.parameter_values
-
     return mpc
 
 
@@ -104,11 +99,6 @@ def test_mpc_initializes():
     assert mpc.ocp_solver.acados_ocp.model is not None
     assert mpc.ocp_solver.acados_ocp.dims is not None
     assert mpc.ocp_solver.acados_ocp.cost is not None
-
-
-def test_get_param_does_not_give_None():
-    # Use the existing ocp solver
-    assert AcadosMPC(param, discount_factor=0.99, **kwargs).get_p() is not None
 
 
 def test_set_p_get_p():
@@ -138,8 +128,6 @@ def test_v_update(
 ):
     mpc = set_up_mpc(generate_code, build_code, json_file_prefix)
 
-    mpc.ocp_solver.solve()
-
     test_param = set_up_test_parameters(mpc, np_test, varying_param_label=varying_param_label)
 
     absolute_difference = run_test_v_update_for_varying_parameters(mpc, x0, test_param, plot)
@@ -161,10 +149,6 @@ def test_q_update(
 
     u0 = mpc.ocp_solver.solve_for_x0(x0)
 
-    # mpc.ocp_solver.solve()
-    # x0 = mpc.ocp_solver.get(0, "x")
-    # u0 = mpc.ocp_solver.get(0, "u")
-
     test_param = set_up_test_parameters(mpc, np_test, varying_param_label=varying_param_label)
 
     absolute_difference = run_test_q_update_for_varying_parameters(mpc, x0, u0, test_param, plot)
@@ -181,11 +165,7 @@ def test_pi_update(
     np_test: int = 10,
     plot: bool = False,
 ):
-    # x0 = np.array([1.0, 1.0])
     mpc = set_up_mpc(generate_code, build_code, json_file_prefix)
-
-    # mpc.ocp_solver.solve()
-    # x0 = mpc.ocp_solver.get(0, "x")
 
     test_param = set_up_test_parameters(mpc, np_test, varying_param_label=varying_param_label)
 
@@ -194,36 +174,44 @@ def test_pi_update(
     assert np.median(absolute_difference) <= 1e-1
 
 
-def test_get_param_returns_correct_shape():
-    # TODO: Implement this test
-    pass
+def test_closed_loop(
+    generate_code: bool = False,
+    build_code: bool = False,
+    json_file_prefix: str = "acados_ocp_linear_system",
+    x0: np.ndarray = np.array([0.5, 0.5]),
+    n_sim: int = 100,
+    plot: bool = False,
+):
+    mpc = set_up_mpc(generate_code, build_code, json_file_prefix)
 
+    x = [x0]
+    u = []
 
-def test_cost_scaling():
-    # TODO: Implement this test. Can use a two-dimensional point mass system with a lower 2-norm constraint away from zero.
-    pass
+    for _ in range(n_sim):
+        u.append(mpc.ocp_solver.solve_for_x0(x[-1]))
+        x.append(mpc.ocp_solver.get(1, "x"))
+        assert mpc.ocp_solver.get_status() == 0
 
+    x = np.array(x)
+    u = np.array(u)
 
-def test_use_existing_ocp_solver():
-    # Generate and build the ocp solver
+    if plot:
+        plt.figure()
+        plt.subplot(3, 1, 1)
+        plt.step(np.arange(n_sim + 1), x[:, 0], label="x_0")
+        plt.subplot(3, 1, 2)
+        plt.step(np.arange(n_sim + 1), x[:, 1], label="x_1")
+        plt.subplot(3, 1, 3)
+        plt.step(np.arange(n_sim), u, label="u")
+        plt.legend()
+        plt.show()
 
-    _ = AcadosMPC(
-        param,
-        discount_factor=0.99,
-        **{
-            "ocp_solver": {"json_file": "acados_ocp.json", "generate": True, "build": True},
-            "ocp_sensitivity_solver": {"json_file": "acados_ocp_sensitivity.json", "generate": True, "build": True},
-        },
-    )
-
-    # Use the existing ocp solver
-    _ = AcadosMPC(param, discount_factor=0.99, **kwargs)
-
-    assert True
+    assert np.median(x[-10:, 0]) <= 1e-1 and np.median(x[-10:, 1]) <= 1e-1 and np.median(u[-10:]) <= 1e-1
 
 
 def main():
-    test_v_update(plot=True, np_test=100, varying_param_label="A_2")
+    test_closed_loop(plot=False)
+    # test_v_update(plot=True, np_test=100, varying_param_label="A_2")
     # test_q_update(plot=True, np_test=100, varying_param_label="A_3")
     # test_pi_update(plot=True, np_test=100)
     # mpc = set_up_mpc()
